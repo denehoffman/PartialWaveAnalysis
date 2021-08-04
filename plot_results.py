58d7e388468200981ebcf5516e18ade22be5845b
denehoffman <denehoffman@me.com>
2021-08-04 15:28:25 -0400

    redo plotting methods

--- plot_results.py
+++ plot_results.py
@@ -24,136 +24,144 @@ def mask_first(x):
 
 mask = df.groupby(['Bin'])['Bin'].transform(mask_first).astype(bool)
 df_filtered = df.loc[mask]
+print(df_filtered.head())
 
 bin_df = pd.read_csv(input_folder / 'bin_info.txt', delimiter='\t')
 amplitudes = [column for column in df.columns[3:-3].to_list()[::2] if not (column.endswith("_re") or column.endswith("_im"))]
-amplitudes_pos = [a for a in amplitudes if a.endswith("+")]
-amplitudes_neg = [a for a in amplitudes if a.endswith("-")]
 amperrors = [column for column in df.columns[3:-3].to_list()[1::2] if not (column.endswith("_re") or column.endswith("_im"))]
-amperrors_pos = [a for a in amperrors if a.endswith("+_err")]
-amperrors_neg = [a for a in amperrors if a.endswith("-_err")]
 
-n_amps = max(len(amplitudes_pos), len(amplitudes_neg))
+wave_set = set([amp[:-1] for amp in amplitudes])
+wave_dict = {'S': 0, 'P': 1, 'D': 2, 'F': 3, 'G': 4}
+waves_sorted = sorted(list(wave_set), key=lambda wave: 100 * wave_dict[wave[0]] + (-1 if wave[-1] == '-' else 1) * int(wave[1]))
+# kind of sneaky wave of sorting waves by L-letter and M-number without creating a whole new class
 
-nrows = int(np.ceil(np.sqrt(n_amps)))
+
 plt.rcParams["figure.figsize"] = (20, 10)
 plt.rcParams["font.size"] = 24
-fig, axes = plt.subplots(nrows=nrows, ncols=nrows)
-indexes = [idx for idx in np.ndindex(axes.shape)]
 
 print("Plotting Separate Amplitudes")
-# Positive
-for i in range(len(amplitudes_pos)):
-    print(amplitudes_pos[i] + "\t±\t" + amperrors_pos[i])
-    axes[indexes[i]].errorbar(bin_df['mass'].iloc[df_filtered['Bin']], df_filtered[amplitudes_pos[i]], yerr=df_filtered[amperrors_pos[i]], elinewidth=0.5, fmt='.', color='r', label=r'$+\epsilon$')
-    amp_letter = amplitudes_pos[i].split("::")[-1][0]
-    amp_m = amplitudes_pos[i].split("::")[-1][1]
+for wave in waves_sorted:
+    fig = plt.figure()
+    print(wave, end='\t')
+    # wave = S0, P1+, D2-, etc. -- no reflectivity info
+    amp_letter = wave[0]
+    amp_m = wave[1]
     if int(amp_m) > 0:
-        amp_m_sign = amplitudes_pos[i].split("::")[-1][2]
+        amp_m_sign = wave[2]
     else:
         amp_m_sign = ""
-    axes[indexes[i]].set_title(rf"${amp_letter}_{{{amp_m_sign}{amp_m}}}$")
 
-# Negative
-for i in range(len(amplitudes_neg)):
-    print(amplitudes_neg[i] + "\t±\t" + amperrors_neg[i])
-    axes[indexes[i]].errorbar(bin_df['mass'].iloc[df_filtered['Bin']], df_filtered[amplitudes_neg[i]], yerr=df_filtered[amperrors_neg[i]], elinewidth=0.5, fmt='.', color='b', label=r'$-\epsilon$')
-    amp_letter = amplitudes_neg[i].split("::")[-1][0]
-    amp_m = amplitudes_neg[i].split("::")[-1][1]
-    if int(amp_m) > 0:
-        amp_m_sign = amplitudes_neg[i].split("::")[-1][2]
+    wave_pos = wave + "+"
+    if wave_pos in amplitudes:
+        print("+e", end='\t')
+        plt.errorbar(bin_df['mass'], df_filtered[wave_pos], yerr=df_filtered[wave_pos + "_err"], elinewidth=0.5, fmt='.', color='r', label=r'$+\epsilon$')
     else:
-        amp_m_sign = ""
-    axes[indexes[i]].set_title(rf"${amp_letter}_{{{amp_m_sign}{amp_m}}}$")
+        print("", end='\t')
 
-for i in range(n_amps):
-    axes[indexes[i]].errorbar(bin_df['mass'].iloc[df_filtered['Bin']], df_filtered['total_intensity'], yerr=df_filtered['total_intensity_err'], elinewidth=0.5, fmt='.', color='k', label='Total')
-    axes[indexes[i]].set_ylim(bottom=0)
-    axes[indexes[i]].set_xlim(bin_df['mass'].iloc[0] - 0.1, bin_df['mass'].iloc[-1] + 0.1)
-    axes[indexes[i]].set_ylabel("Intensity")
-    axes[indexes[i]].set_xlabel(xlabel)
-    axes[indexes[i]].legend()
-
-plt.tight_layout()
-pdf.savefig(fig, dpi=300)
+    wave_neg = wave + "-"
+    if wave_neg in amplitudes:
+        print("-e", end='\t')
+        plt.errorbar(bin_df['mass'], df_filtered[wave_neg], yerr=df_filtered[wave_neg + "_err"], elinewidth=0.5, fmt='.', color='b', label=r'$-\epsilon$')
+    else:
+        print("", end='\t')
+    # Plot total
+    print("tot")
+    plt.errorbar(bin_df['mass'], df_filtered['total_intensity'], yerr=df_filtered['total_intensity_err'], elinewidth=0.5, fmt='.', color='k', label='Total')
+    plt.title(rf"${amp_letter}_{{{amp_m_sign}{amp_m}}}$")
+    plt.ylim(bottom=0)
+    plt.xlim(bin_df['mass'].iloc[0] - 0.1, bin_df['mass'].iloc[-1] + 0.1)
+    plt.ylabel("Intensity")
+    plt.xlabel(xlabel)
+    plt.legend()
+    plt.tight_layout()
+    pdf.savefig(fig, dpi=300)
 
 colors = ['aqua', 'blue', 'chartreuse', 'coral', 'crimson', 'darkblue', 'darkgreen', 'fuchsia', 'gold', 'indigo', 'lime', 'orangered', 'teal', 'sienna']
 
 print("Plotting Combined Amplitudes")
+print("Positive Reflectivity")
 fig = plt.figure()
-if len(amplitudes_pos) != 0:
-    print("Positive Only")
-    for i in range(len(amplitudes_pos)):
-        print(amplitudes_pos[i] + "\t±\t" + amperrors_pos[i])
-        amp_letter = amplitudes_pos[i].split("::")[-1][0]
-        amp_m = amplitudes_pos[i].split("::")[-1][1]
-        if int(amp_m) > 0:
-            amp_m_sign = amplitudes_pos[i].split("::")[-1][2]
-        else:
-            amp_m_sign = ""
-        plt.errorbar(bin_df['mass'].iloc[df_filtered['Bin']], df_filtered[amplitudes_pos[i]], yerr=df_filtered[amperrors_pos[i]], elinewidth=0.5, fmt='.', color=colors[i], label=rf"${amp_letter}_{{{amp_m_sign}{amp_m}}}$")
-    plt.errorbar(bin_df['mass'].iloc[df_filtered['Bin']], df_filtered['total_intensity'], yerr=df_filtered['total_intensity_err'], elinewidth=0.5, fmt='.', color='k', label="Total")
-    plt.xlim(bin_df['mass'].iloc[0] - 0.1, bin_df['mass'].iloc[-1] + 0.1)
-    plt.ylim(bottom=-100)
-    plt.legend(loc="upper right")
-    plt.axhline(0, color='k')
-    plt.title("Positive Reflectivity")
-    plt.ylabel("Intensity")
-    plt.xlabel(xlabel)
-    plt.tight_layout()
-    pdf.savefig(fig, dpi=300)
- 
-fig = plt.figure()
-if len(amplitudes_neg) != 0:
-    print("Negative Only")
-    for i in range(len(amplitudes_neg)):
-        print(amplitudes_neg[i] + "\t±\t" + amperrors_neg[i])
-        amp_letter = amplitudes_neg[i].split("::")[-1][0]
-        amp_m = amplitudes_neg[i].split("::")[-1][1]
-        if int(amp_m) > 0:
-            amp_m_sign = amplitudes_neg[i].split("::")[-1][2]
-        else:
-            amp_m_sign = ""
-        plt.errorbar(bin_df['mass'].iloc[df_filtered['Bin']], df_filtered[amplitudes_neg[i]], yerr=df_filtered[amperrors_neg[i]], elinewidth=0.5, fmt='.', color=colors[i], label=rf"${amp_letter}_{{{amp_m_sign}{amp_m}}}$")
-    plt.errorbar(bin_df['mass'].iloc[df_filtered['Bin']], df_filtered['total_intensity'], yerr=df_filtered['total_intensity_err'], elinewidth=0.5, fmt='.', color='k', label="Total")
-    plt.xlim(bin_df['mass'].iloc[0] - 0.1, bin_df['mass'].iloc[-1] + 0.1)
-    plt.ylim(bottom=-100)
-    plt.legend(loc="upper right")
-    plt.axhline(0, color='k')
-    plt.title("Negative Reflectivity")
-    plt.ylabel("Intensity")
-    plt.xlabel(xlabel)
-    plt.tight_layout()
-    pdf.savefig(fig, dpi=300)
- 
-print("Positive and Negative Combined")
-fig = plt.figure()
-for i in range(len(amplitudes_pos)):
-    print(amplitudes_pos[i] + "\t±\t" + amperrors_pos[i])
-    amp_letter = amplitudes_pos[i].split("::")[-1][0]
-    amp_m = amplitudes_pos[i].split("::")[-1][1]
+for i, wave in enumerate(waves_sorted):
+    amp_letter = wave[0]
+    amp_m = wave[1]
     if int(amp_m) > 0:
-        amp_m_sign = amplitudes_pos[i].split("::")[-1][2]
+        amp_m_sign = wave[2]
     else:
         amp_m_sign = ""
-    plt.errorbar(bin_df['mass'].iloc[df_filtered['Bin']], df_filtered[amplitudes_pos[i]], yerr=df_filtered[amperrors_pos[i]], elinewidth=0.5, fmt='.', color=colors[i], label=rf"${amp_letter}^+_{{{amp_m_sign}{amp_m}}}$")
 
-for i in range(len(amplitudes_neg)):
-    print(amplitudes_neg[i] + "\t±\t" + amperrors_neg[i])
-    amp_letter = amplitudes_neg[i].split("::")[-1][0]
-    amp_m = amplitudes_neg[i].split("::")[-1][1]
-    if int(amp_m) > 0:
-        amp_m_sign = amplitudes_neg[i].split("::")[-1][2]
-    else:
-        amp_m_sign = ""
-    plt.errorbar(bin_df['mass'].iloc[df_filtered['Bin']], df_filtered[amplitudes_neg[i]], yerr=df_filtered[amperrors_neg[i]], elinewidth=0.5, fmt='d', color=colors[i], label=rf"${amp_letter}^-_{{{amp_m_sign}{amp_m}}}$")
-
-plt.errorbar(bin_df['mass'].iloc[df_filtered['Bin']], df_filtered['total_intensity'], yerr=df_filtered['total_intensity_err'], elinewidth=0.5, fmt='.', color='k', label="Total")
+    wave_pos = wave + "+"
+    if wave_pos in amplitudes:
+        print(wave + "\t+e", end="\t")
+        plt.errorbar(bin_df['mass'], df_filtered[wave_pos], yerr=df_filtered[wave_pos + "_err"], elinewidth=0.5, fmt='.', color=colors[i], label=rf"${amp_letter}_{{{amp_m_sign}{amp_m}}}$")
+print("tot")
+plt.errorbar(bin_df['mass'], df_filtered['total_intensity'], yerr=df_filtered['total_intensity_err'], elinewidth=0.5, fmt='.', color='k', label="Total")
 plt.xlim(bin_df['mass'].iloc[0] - 0.1, bin_df['mass'].iloc[-1] + 0.1)
 plt.ylim(bottom=-100)
 plt.legend(loc="upper right")
 plt.axhline(0, color='k')
-plt.title("Fit Results")
+plt.title("Positive Reflectivity")
+plt.ylabel("Intensity")
+plt.xlabel(xlabel)
+plt.tight_layout()
+pdf.savefig(fig, dpi=300)
+
+print("Negative Reflectivity")
+fig = plt.figure()
+for i, wave in enumerate(waves_sorted):
+    amp_letter = wave[0]
+    amp_m = wave[1]
+    if int(amp_m) > 0:
+        amp_m_sign = wave[2]
+    else:
+        amp_m_sign = ""
+
+    wave_neg = wave + "-"
+    if wave_neg in amplitudes:
+        print(wave + "\t\t-e", end="\t")
+        plt.errorbar(bin_df['mass'], df_filtered[wave_neg], yerr=df_filtered[wave_neg + "_err"], elinewidth=0.5, fmt='.', color=colors[i], label=rf"${amp_letter}_{{{amp_m_sign}{amp_m}}}$")
+print("tot")
+plt.errorbar(bin_df['mass'], df_filtered['total_intensity'], yerr=df_filtered['total_intensity_err'], elinewidth=0.5, fmt='.', color='k', label="Total")
+plt.xlim(bin_df['mass'].iloc[0] - 0.1, bin_df['mass'].iloc[-1] + 0.1)
+plt.ylim(bottom=-100)
+plt.legend(loc="upper right")
+plt.axhline(0, color='k')
+plt.title("Negative Reflectivity")
+plt.ylabel("Intensity")
+plt.xlabel(xlabel)
+plt.tight_layout()
+pdf.savefig(fig, dpi=300)
+
+print("Positive and Negative Reflectivity")
+fig = plt.figure()
+for i, wave in enumerate(waves_sorted):
+    amp_letter = wave[0]
+    amp_m = wave[1]
+    if int(amp_m) > 0:
+        amp_m_sign = wave[2]
+    else:
+        amp_m_sign = ""
+
+    wave_pos = wave + "+"
+    print(wave, end='\t')
+    if wave_pos in amplitudes:
+        print("+e", end='\t')
+        plt.errorbar(bin_df['mass'], df_filtered[wave_pos], yerr=df_filtered[wave_pos + "_err"], elinewidth=0.5, fmt='.', color=colors[i], label=rf"${amp_letter}^+_{{{amp_m_sign}{amp_m}}}$")
+    else:
+        print("", end='\t')
+
+    wave_neg = wave + "-"
+    if wave_neg in amplitudes:
+        print("-e", end='\t')
+        plt.errorbar(bin_df['mass'], df_filtered[wave_neg], yerr=df_filtered[wave_neg + "_err"], elinewidth=0.5, fmt='s', color=colors[i], label=rf"${amp_letter}^-_{{{amp_m_sign}{amp_m}}}$")
+    else:
+        print("", end='\t')
+print("tot")
+plt.errorbar(bin_df['mass'], df_filtered['total_intensity'], yerr=df_filtered['total_intensity_err'], elinewidth=0.5, fmt='.', color='k', label="Total")
+plt.xlim(bin_df['mass'].iloc[0] - 0.1, bin_df['mass'].iloc[-1] + 0.1)
+plt.ylim(bottom=-100)
+plt.legend(loc="upper right")
+plt.axhline(0, color='k')
+plt.title("All Waves")
 plt.ylabel("Intensity")
 plt.xlabel(xlabel)
 plt.tight_layout()
